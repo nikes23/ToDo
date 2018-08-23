2fc405cd560dd3c89ce167229239940c8159c35a
nikes23 <ank9590@thi.de>
2018-08-23 23:22:53 +0200

    Convert to MVC design pattern

---
+++ ToDo/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  ToDo
+//
+//  Created by Anton Kempf on 21.08.18.
+//  Copyright © 2018 Anton Kempf. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
